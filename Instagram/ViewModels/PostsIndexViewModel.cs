ee9954e5a9c129f09cd5938099a32d4e30f1490f
Assylbekova Gulzhan <123gulzhan@gmail.com>
2021-04-02 19:03:26 +0600

    удалены комментарии

--- Instagram/ViewModels/PostsIndexViewModel.cs
+++ Instagram/ViewModels/PostsIndexViewModel.cs
@@ -6,6 +6,5 @@ namespace Instagram.ViewModels
     public class PostsIndexViewModel
     {
         public IQueryable<Post> Posts { get; set; }
-        //public IQueryable<Comment> Comments { get; set; }
     }
 }
